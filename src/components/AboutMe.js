4821ebe8efc99a59adf09eb5382f9c0fdce0a68e
Henry Ni <henryni914@gmail.com>
2020-04-29 17:16:57 -0400

    added buttons in navbar

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -3,6 +3,9 @@ import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import { Typography } from '@material-ui/core';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -14,6 +17,11 @@ const useStyles = makeStyles((theme) => ({
         // color: theme.palette.text.secondary,
         marginTop: theme.spacing(8)
     },
+    media: {
+        height: 200,
+        width: 200,
+        position: 'relative'
+    }
 }));
 
 export default function CenteredGrid() {
@@ -23,9 +31,27 @@ export default function CenteredGrid() {
         <div className={classes.root}>
             <Grid container spacing={3} mt={3}>
                 <Grid item xs={12}>
-                    <Paper className={classes.paper} elevation={0}>
+                    <Card className={classes.paper} elevation={1}>
+                        <div>
+                            <CardContent display='flex'>
+                                <Typography variant='h5'>
+                                    Start of About Me!
+                            </Typography>
+                                <Typography>
+                                    Hello, I'm Henry and I'm an aspiring web developer with
+                            </Typography>
+                            </CardContent>
+                        </div>
+                        <CardMedia
+                            display='flex'
+                            className={classes.media}
+                            component='img'
+                            src={"https://source.unsplash.com/random"}
+                        ></CardMedia>
+                    </Card>
+                    {/* <Paper className={classes.paper} elevation={0}>
                         <h2>About Me!</h2>
-                    </Paper>
+                    </Paper> */}
                 </Grid>
             </Grid>
         </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,36 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Typography from '@material-ui/core/Typography';
 import Toolbar from '@material-ui/core/Toolbar';
 import Button from '@material-ui/core/Button';
 import { Link } from "react-router-dom";
 
+const useStyles = makeStyles((theme) => ({
+    button: {
+        align: 'right',
+        marginRight: theme.spacing(2)
+    }
+}))
+
 export default function Navbar() {
+
+    const classes = useStyles();
 
     return (
         <AppBar>
-            
+
             <Toolbar>
-            <Typography>Henry Ni's Portfolio</Typography>
+                <Typography align="center">Henry Ni's Portfolio</Typography>
+                <Button className={classes.button}>
+                    About Me
+            </Button>
+                <Button>
+                    Projects
+            </Button>
+                <Button>
+                    Resume
+            </Button>
                 {/* <Link to="/">
                     <Button>Home</Button>
                 </Link>
